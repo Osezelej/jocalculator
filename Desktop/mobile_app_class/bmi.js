95c32e4d6bcecef2d38fbf90e4a69ffed7c292ce
osezelej <2osezelejoseph@gmail.com>
2022-11-18 12:40:04 +0100

    Bmi completed

---
+++ Desktop/mobile_app_class/bmi.js
@@ -0,0 +1,24 @@
+$('button').click(handleClick)
+function handleClick (){
+    $('button').css('background-color', )
+    let weight = $('.weight_input').val()
+    let height = $('.height_input').val()
+
+    let BMI = weight / (height * height)
+    console.log(BMI)
+    if (BMI < 18.6){
+        $('.results').text('UNDERWEIGHT')
+        $('.results').css('color', 'yellow')
+        $('.results').css('font-weight', '900')
+    }else if(BMI >=18.6 && BMI <= 24.9 ){
+        $('.results').text('HEALTHY')
+        $('.results').css('color', 'green')
+        $('.results').css('font-weight', '900')
+
+    }else{
+        $('.results').text('OVERWEIGHT')
+        $('.results').css('color', 'red')
+        $('.results').css('font-weight', '900')
+    }
+}
+
